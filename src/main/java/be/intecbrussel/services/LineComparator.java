cb80bf46b3971428f59fad7e983b03e18cd6b8ac
erikh <91531357+erikholl@users.noreply.github.com>
2022-04-25 17:53:37 +0200

    beginning of useful tests included - yes, i know, slow..

--- src/main/java/be/intecbrussel/services/LineComparator.java
+++ src/main/java/be/intecbrussel/services/LineComparator.java
@@ -9,55 +9,82 @@ public class LineComparator {
     // performance: perform length check, only if length is same, create
     // combi string and check content of strings
 
-    List<String> fullList;
-    List<String> listToCompareTo;
-
-    String stringToCheck;
-
-    int lineNrStringToCheck;
+    private List<String> fullList;
+    private List<String> listToCompareTo;
+    private String stringToCheck;
+    private int lineNrStringToCheck;
 
     public void setFullList(List<String> list) {
         this.fullList = list;
     }
 
+    public List<String> getFullList() {
+        return fullList;
+    }
+
+    public List<String> getListToCompareTo() {
+        return listToCompareTo;}
+
+    public void setListToCompareTo(List<String> list, int index) {
+        List<String> listToCompareTo = new ArrayList<>(list);
+        listToCompareTo.remove(index);             //removes String @i (String
+        // to check
+        listToCompareTo.add(index, "");    // adds "" at i, moves all
+        // elements right of it 1 place to the right - keeps original
+        // length, important for printing the line nr later on
+        this.listToCompareTo = listToCompareTo;
+    }
+
+    public String getStringToCheck() {
+        return stringToCheck;
+    }
+
+    public void setStringToCheck(String stringToCheck) {
+        this.stringToCheck = stringToCheck;
+    }
+
+    public int getLineNrStringToCheck() {
+        return lineNrStringToCheck;
+    }
+
+    public void setLineNrStringToCheck(int lineNrStringToCheck) {
+        this.lineNrStringToCheck = lineNrStringToCheck + 1;
+    }
+
     public void runThroughListCheckCombinations() {
         for (int i = 0; i < fullList.size(); i++) {
-            setLineAndListToPerformComparisonOn(fullList, i);
+            setStringToCheck(fullList.get(i));
+            setLineNrStringToCheck(i);
+            setListToCompareTo(fullList, i);
             comparison(stringToCheck, listToCompareTo);
         }
     }
 
-    // private 'setter' for both line x and new list to compare line x with
-    private void setLineAndListToPerformComparisonOn(List<String> fullList,
-                                                     int index) {
-        this.stringToCheck = fullList.get(index);
-        this.lineNrStringToCheck = index + 1;
-        listToCompareTo = new ArrayList<>(fullList);
-        listToCompareTo.remove(index);
-    }
-
-    private void comparison(String s, List<String> l) {
-        for (int i = 0; i < l.size() - 1; i++) {
+    int comparison(String s, List<String> l) {
+        int count = 0;
+        for (int i = 0; i < l.size(); i++) {
             if (l.get(i).trim().length() == 0) continue;
 
             for (int j = 0; j < l.size(); j++) {
                 // avoid picking the same element twice:
                 if (j == i) continue;
-                // skip lines with only spaces:
+                // skip lines with only spaces / no content:
                 if (l.get(j).trim().length() == 0) continue;
 
                 if (lengthOfStringsIsSame(l.get(i), l.get(j), s)) {
                     String c = l.get(i) + l.get(j);
 
                     if (c.equals(s)) {
-                        System.out.println("for line " + lineNrStringToCheck + " '" + s + "' found combination on line " + i + " '" + l.get(i) + "' + line " + j + " '" + l.get(j) +"'");
+                        count++;
+                        System.out.println("for line " + lineNrStringToCheck + " '" + s + "' found combination on line " + (i + 1) + " '" + l.get(i) + "' + line " + (j + 1) + " '" + l.get(j) +"'");
                     }
                 }
             }
         }
+        return count;
     }
 
-    private boolean lengthOfStringsIsSame(String s1, String s2, String main) {
+    boolean lengthOfStringsIsSame(String s1, String s2, String main) {
         return s1.length() + s2.length() == main.length();
     }
 }
--- src/test/java/be/intecbrussel/services/LineComparatorTest.java
+++ src/test/java/be/intecbrussel/services/LineComparatorTest.java
@@ -1,24 +1,92 @@
 package be.intecbrussel.services;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+// tests TODO:
+// no list or an empty list?
+// array out of bounds?
+// using MethodSource, but for what, a List with Lists of Strings maybe?
+// look into stream?
+// other?
+
+
 class LineComparatorTest {
-    String testString = "bob";
-    List<String> testList = Arrays.asList("bb", "b", "bo", "b");
-    LineComparator lc;
+    final String S1 = "bob";
+    final List<String> FULLLIST = Arrays.asList("bb", "b", "bo", "b");
+    final List<String> EMPTY = Arrays.asList();
+    final LineComparator LC = new LineComparator();
+
+    @BeforeEach
+    void init() {
+        System.out.println("init...");
+        // but how to really init?
+    }
+
+    @AfterEach
+    void exit() {
+        System.out.println("after test...");
+    }
 
     @Test
-    void runThroughFileAndCheckCombinationsEmptyList() {
-        List<String> empty = Arrays.asList("");
-        lc.runThroughListCheckCombinations(); // argh, not possible,
-        // help-methods.. probably need to adapt whole structure :(
-        // need to clarify.
-        // also, how on earth to test file readers??
-
+    void testFullListSetGet() {
+        // when
+        LC.setFullList(FULLLIST);
+        // then
+        assertThat(FULLLIST, is(LC.getFullList()));
     }
+
+    @Test
+    void testListToCompareToSetGet() {
+        final List<String> COMPARELIST = Arrays.asList("bb", "", "bo", "b");
+        final int INDEX = 1;
+
+        // when
+        LC.setListToCompareTo(FULLLIST, INDEX);
+        // then
+        assertThat(COMPARELIST, is(LC.getListToCompareTo()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void paramTestStringSetGet(int index) {
+        // when
+        LC.setStringToCheck(FULLLIST.get(index));
+        // and
+        final String OUTPUT = LC.getStringToCheck();
+        // then
+        assertEquals(FULLLIST.get(index), OUTPUT);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void paramTestLineNr(int index) {
+        // when
+        LC.setLineNrStringToCheck(index);
+        // and
+        int lineNr = index + 1;
+        // then
+        assertEquals(lineNr, LC.getLineNrStringToCheck());
+    }
+
+    @Test
+    void testComparison() {
+        final List<String> LIST = Arrays.asList("bob", "b", "oo", "dodo", "b");
+        final String WORD = "boo";
+        final int COUNT = 2;
+        // when
+        int comparisonCount = LC.comparison(WORD, LIST);
+        assertEquals(COUNT, comparisonCount);
+    }
+
 }
